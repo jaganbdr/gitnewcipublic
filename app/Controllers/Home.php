992587fb6491c23cec3d063a7944591dbba5a1b1
jaganbdr <jagan.mains@gmail.com>
2022-11-28 19:25:37 +0530

    added new echo

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -6,7 +6,8 @@ class Home extends BaseController
 {
     public function index()
     {
-        return view('welcome_message');
         echo "this is updated";
+        return view('welcome_message');
+        
     }
 }
